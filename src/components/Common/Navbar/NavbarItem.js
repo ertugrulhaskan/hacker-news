835b3301a7d42a162d121c244b21037f4d437cd9
Ertugrul Alex Haskan <ertughaskan@gmail.com>
2022-09-14 01:57:03 -0700

    light mode color changes

--- src/components/Common/Navbar/NavbarItem.js
+++ src/components/Common/Navbar/NavbarItem.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavbarItem = (props) => {
   return (
-    <div className="inline-block h-10 w-10 cursor-pointer rounded-full transition-all duration-300 hover:bg-gray-200 lg:h-auto lg:w-auto lg:py-2 lg:px-4">
+    <div className="inline-block h-10 w-10 cursor-pointer rounded-full transition-all duration-300 hover:bg-orange-200 dark:hover:bg-stone-700 lg:h-auto lg:w-auto lg:py-2 lg:px-4">
       <div className="flex h-full flex-row items-center justify-center">
         <div className="w-6">{props.children}</div>
         <div className="ml-3 hidden lg:inline-block">{props.text}</div>
