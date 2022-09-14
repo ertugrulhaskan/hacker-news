6eb30ffeccaede5fb6175c1034f0658402611191
Ertugrul Alex Haskan <ertughaskan@gmail.com>
2022-09-14 02:01:55 -0700

    light mode color changes

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -7,12 +7,12 @@ const Box = ({ feed }) => {
       href={feed.url || `https://news.ycombinator.com/item?id=${feed.id}`}
       target="_blank"
       rel="noreferrer"
-      className="block border-b border-orange-100 p-4 last:border-none visited:text-fuchsia-700 hover:bg-orange-100 dark:border-gray-800 dark:visited:text-fuchsia-600 dark:hover:bg-gray-800"
+      className="block border-b border-orange-100 p-4 last:border-none visited:text-fuchsia-700 hover:bg-orange-100 dark:border-stone-800 dark:visited:text-fuchsia-600 dark:hover:bg-stone-800"
     >
       <div className="font-semibold">{feed.title}</div>
       <>
         <div
-          className="my-2 hidden h-20 overflow-hidden text-clip rounded-lg border border-gray-200 bg-gray-50 p-2 font-mono text-xs leading-loose"
+          className="my-2 hidden h-20 overflow-hidden text-clip rounded-lg border border-stone-200 bg-stone-50 p-2 font-mono text-xs leading-loose"
           dangerouslySetInnerHTML={{
             __html: `<p>something<p>`,
           }}
