e8e6e0fbb02b2114d937879a0d072350d7f6becb
Ertugrul Alex Haskan <ertughaskan@gmail.com>
2022-09-14 09:26:39 -0700

    Saving theme setting on localStorage

--- src/providers/ThemeContextProvider.js
+++ src/providers/ThemeContextProvider.js
@@ -6,13 +6,16 @@ const ThemeContextProvider = (props) => {
   const [theme, setTheme] = useState("light");
 
   useEffect(() => {
+    const themeSettings = localStorage.getItem("HNTheme") || theme;
+    setTheme(themeSettings);
     document.querySelector("html").removeAttribute("class");
-    document.querySelector("html").classList.add(theme);
+    document.querySelector("html").classList.add(themeSettings);
   }, [theme]);
 
   const changeTheme = () => {
     const selectedTheme = theme === "light" ? "dark" : "light";
     setTheme(selectedTheme);
+    localStorage.setItem("HNTheme", selectedTheme);
   };
 
   return (
